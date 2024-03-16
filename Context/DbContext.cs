db7fd5c5dae1aa1f373b9623ce5971e46db3116f
Jerremy Stroman <jerremystroman@gmail.com>
2024-03-15 21:27:04 -0700

    Add contexts, and user

---
+++ Context/DbContext.cs
@@ -0,0 +1,34 @@
+﻿using GAuth2.Entities;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Options;
+using System.Data;
+
+namespace GAuth2.Context;
+public class GAuthContext
+{
+    private readonly string _connectionString;
+    public GAuthContext(IConfiguration configuration)
+    {
+        _connectionString = configuration.GetConnectionString("GAuth") ??
+            throw new ArgumentNullException("GAuth connection string not found");
+    }
+
+    public IDbConnection CreateConnection()
+    {
+        return new SqlConnection(_connectionString);
+    }
+}
+public class ClinicalDashboardContext
+{
+    private readonly string _connectionString;
+    public ClinicalDashboardContext(IConfiguration configuration)
+    {
+        _connectionString = configuration.GetConnectionString("ClinicalDashboard") ??
+            throw new ArgumentNullException("ClinicalDashboard connection string not found");
+    }
+
+    public IDbConnection CreateConnection()
+    {
+        return new SqlConnection(_connectionString);
+    }
+}
